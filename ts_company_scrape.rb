b155dfe969fcbe6bdf4775b5f997257db6ad0065
Lee Saville <l.w.saville@gmail.com>
2017-03-27 19:41:31 -0600

    Attempt to alter timeout

--- ts_company_scrape.rb
+++ ts_company_scrape.rb
@@ -3,8 +3,14 @@ require 'nokogiri'
 require 'json'
 require 'pry'
 
-page = Faraday.get('http://www.techstars.com/companies/')
-
+conn = Faraday.new(:url => 'http://www.techstars.com/companies/') do |faraday|
+  faraday.request  :url_encoded             # form-encode POST params
+  faraday.response :logger                  # log requests to STDOUT
+  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+end
+page = conn.get do |req|
+  req.options.open_timeout = 10
+end
 p_page = Nokogiri::HTML(page.body)
 
 binding.pry
