de8c635c8afde8e160d644753995c8c9723c0b2e
Lee Saville <l.w.saville@gmail.com>
2017-03-30 15:09:00 -0600

    Uncomment flow. Add Capybara set-up.

--- bi_company_scrape.rb
+++ bi_company_scrape.rb
@@ -2,14 +2,36 @@ require 'faraday'
 require 'nokogiri'
 require 'json'
 require 'pry'
+require 'capybara/poltergeist'
 
-page = Faraday.get('http://www.builtincolorado.com/companies')
-
-p_page = Nokogiri::HTML(page.body)
-
-link_fragments = p_page.css('.views-content .views-field-title a').map do |link|
-  link['href']
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: false)
 end
+
+Capybara.default_driver = :poltergeist
+
+browser = Capybara.current_session
+browser.visit 'http://www.builtincolorado.com/companies#/companies'
+browser.fill_in 'Email Address', with: 'jobseeker.fantastico@gmail.com'
+browser.fill_in 'Password', with: 'my-cat-georgia'
+browser.click_on 'Go!'
+sleep 3
+browser.click_on 'Technology Company'
+
+sleep 3
+links = []
+browser.within 'div.results.companies' do
+  links = browser.all 'a'
+end
+
+company_urls = []
+links.each do |link|
+  company_urls << link['href']
+end
+
+next_button = browser.find 'li.pager-next'
+binding.pry
+next_button.click
 
 binding.pry
 
