31394cfcebc4ce94560677a5b697c39b988f09ca
Lee Saville <l.w.saville@gmail.com>
2017-03-30 15:59:45 -0600

    Start to group in methods. Add sanity checks.

--- bi_company_scrape.rb
+++ bi_company_scrape.rb
@@ -4,34 +4,48 @@ require 'json'
 require 'pry'
 require 'capybara/poltergeist'
 
+# preamble
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, js_errors: false)
 end
 
 Capybara.default_driver = :poltergeist
 
-browser = Capybara.current_session
-browser.visit 'http://www.builtincolorado.com/companies#/companies'
-browser.fill_in 'Email Address', with: 'jobseeker.fantastico@gmail.com'
-browser.fill_in 'Password', with: 'my-cat-georgia'
-browser.click_on 'Go!'
-sleep 3
-browser.click_on 'Technology Company'
+@browser = Capybara.current_session
+@browser.visit 'http://www.builtincolorado.com/companies#/companies'
+@browser.fill_in 'Email Address', with: 'jobseeker.fantastico@gmail.com'
+@browser.fill_in 'Password', with: 'my-cat-georgia'
+@browser.click_on 'Go!'
+sleep 4
+@browser.click_on 'Technology Company'
 
-sleep 3
-links = []
-browser.within 'div.results.companies' do
-  links = browser.all 'a'
+def scrape_company_urls(company_urls=[])
+  sleep 3
+  pagination = @browser.find 'li.pager-current'
+  page_num = pagination.text.to_i
+  binding.pry
+  puts "I'm on page #{page_num + 1}"
+  links = []
+  @browser.within 'div.results.companies' do
+    links = @browser.all 'a'
+  end
+
+  links.each do |link|
+    company_urls << link['href']
+  end
+  puts "Finished with this page on to #{page_num + 2}"
+  puts "There have been #{company_urls.count} company urls gathered"
+  next_button = @browser.find 'li.pager-next'
+  binding.pry
+  next_button ? click_next_and_scrape(company_urls, next_button) : company_urls
 end
 
-company_urls = []
-links.each do |link|
-  company_urls << link['href']
+def click_next_and_scrape(company_urls, next_button)
+  next_button.click
+  scrape_company_urls(company_urls)
 end
 
-next_button = browser.find 'li.pager-next'
-binding.pry
-next_button.click
+blah = scrape_company_urls
 
 binding.pry
 
