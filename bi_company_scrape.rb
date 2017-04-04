f6cbe614a4c39c94a2bc4a340b1a7a47d1d10137
Lee Saville <l.w.saville@gmail.com>
2017-04-04 17:10:37 -0600

    Reorganize code

--- bi_company_scrape.rb
+++ bi_company_scrape.rb
@@ -2,56 +2,70 @@ require 'faraday'
 require 'nokogiri'
 require 'json'
 require 'pry'
-require 'capybara/poltergeist'
+require './setup-capybara'
 
-# preamble
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, js_errors: false)
-end
-
-Capybara.default_driver = :poltergeist
-
-@browser = Capybara.current_session
-@browser.visit 'http://www.builtincolorado.com/companies#/companies'
-@browser.fill_in 'Email Address', with: 'jobseeker.fantastico@gmail.com'
-@browser.fill_in 'Password', with: 'my-cat-georgia'
-@browser.click_on 'Go!'
-sleep 5
-@browser.click_on 'Technology Company'
-
-def scrape_company_urls(company_urls=[])
-  sleep 5
-  pagination = @browser.find 'li.pager-current'
-  page_num = pagination.text.to_i
-  puts "I'm on page #{page_num + 1}"
-  links = []
-  @browser.within 'div.results.companies' do
-    links = @browser.all 'a'
+class BiCompanyScrape
+  
+  def initialize
+    @browser = Capybara.current_session
+  end
+  
+  def self.scrape
+    scraper = BiCompanyScrape.new
+    scraper.sign_on_and_setup
+    scraper.scrape_company_urls
   end
 
-  links.each do |link|
-    company_urls << link['href']
+  def sign_on_and_setup
+    @browser.visit 'http://www.builtincolorado.com/companies#/companies'
+    @browser.fill_in 'Email Address', with: 'jobseeker.fantastico@gmail.com'
+    @browser.fill_in 'Password', with: 'my-cat-georgia'
+    @browser.click_on 'Go!'
+    sleep 5
+    @browser.click_on 'Technology Company'
   end
-  puts "Finished with this page on to #{page_num + 2}"
-  puts "There have been #{company_urls.count} company urls gathered"
-  next_button = find_next_button
-  next_button ? click_next_and_scrape(company_urls, next_button) : company_urls
-end
 
-def find_next_button
-  if @browser.has_css? 'li.pager-next'
-    @browser.find 'li.pager-next'
-  else
-    nil
+  def scrape_company_urls(company_urls=[])
+    sleep 10
+    anchors = anchors_from_page
+    company_urls + urls_from_anchors(anchors)
+    next_if_possible(company_urls)
+  end
+
+  def next_if_possible(company_urls)
+    next_button = find_next_button
+    next_button ? click_next_and_scrape(company_urls, next_button) : company_urls
+  end
+
+  def urls_from_anchors(anchors)
+    anchors.map do |link|
+      link['href']
+    end
+  end
+
+  def anchors_from_page
+    anchors = []
+    @browser.within 'div.results.companies' do
+      anchors = @browser.all 'a'
+    end
+    anchors
+  end
+
+  def find_next_button
+    if @browser.has_css? 'li.pager-next'
+      @browser.find 'li.pager-next'
+    else
+      nil
+    end
+  end
+
+  def click_next_and_scrape(company_urls, next_button)
+    next_button.click
+    scrape_company_urls(company_urls)
   end
 end
 
-def click_next_and_scrape(company_urls, next_button)
-  next_button.click
-  scrape_company_urls(company_urls)
-end
-
-blah = scrape_company_urls
+blah = BiCompanyScrape.scrape
 
 binding.pry
 
